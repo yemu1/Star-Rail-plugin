4145ef0098f1ccb407a8c4b28971d4cf75e573b8
YeMu <qinyinsu@gmail.com>
2025-01-17 22:59:38 +0800

    Initial commit

--- src/index.ts
+++ src/index.ts
@@ -7,5 +7,10 @@ export interface Config {}
 export const Config: Schema<Config> = Schema.object({})
 
 export function apply(ctx: Context) {
-  // write your plugin here
+    // 如果收到“天王盖地虎”，就回应“宝塔镇河妖”
+    ctx.on('message', (session) => {
+      if (session.content === '天王盖地虎') {
+        session.send('宝塔镇河妖')
+      }
+    })
 }
